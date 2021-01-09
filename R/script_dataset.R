68b7c06965747bc302dd595b223e7eb8ab45998f
Ícaro Vidal Freire <icarofreire7@gmail.com>
2021-01-09 00:21:02 -0300

    - muitos datasets    - preciso conhecer para selecionar    - apaguei o script

--- R/script_dataset.R
+++ R/script_dataset.R
@@ -1,11 +1,5 @@
 rm(list = ls())
 library(tidyverse)
 
-dados <- as.tibble(airmiles) %>%
-  mutate(
-    ano = seq(1937, 1960, 1),
-    milhas = x
-  ) %>%
-  select(ano, milhas)
 
-# Não consegui transformar em `data`
+
